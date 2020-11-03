8c84c26a2f1e1011c611e92737655245acbd7739
Adesh <adesh.kadambi@mail.utoronto.ca>
2020-11-03 18:48:47 -0500

    new angular application + firebase deploy

---
+++ egoviz-cdss/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAs08qe9PpkYFjOyej7a7qfdfYpwdbboFg",
+    authDomain: "egoviz.firebaseapp.com",
+    databaseURL: "https://egoviz.firebaseio.com",
+    projectId: "egoviz",
+    storageBucket: "egoviz.appspot.com",
+    messagingSenderId: "1:893270688549:web:792b8595bac88e2433d0cc",
+    appId: '[WEB_APP_ID]'
+  }
+};
---
+++ egoviz-cdss/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAs08qe9PpkYFjOyej7a7qfdfYpwdbboFg",
+    authDomain: "egoviz.firebaseapp.com",
+    databaseURL: "https://egoviz.firebaseio.com",
+    projectId: "egoviz",
+    storageBucket: "egoviz.appspot.com",
+    messagingSenderId: "1:893270688549:web:792b8595bac88e2433d0cc",
+    appId: '[WEB_APP_ID]'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
