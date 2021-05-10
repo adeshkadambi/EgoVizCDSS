7a36ef36a7c87eda856f61dcb7ef6143a5e628b5
Adesh Kadambi <adeshkadambi@gmail.com>
2021-05-10 19:07:30 -0400

    cdss-angular upload

---
+++ cdss-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { AngularFireModule } from '@angular/fire';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cdss-angular/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAiE5T_ybGi0oQDBwOWxso3S4ghxZwhgwA",
+    authDomain: "ego-vision.firebaseapp.com",
+    databaseURL: "https://ego-vision.firebaseio.com",
+    projectId: "ego-vision",
+    storageBucket: "ego-vision.appspot.com",
+    messagingSenderId: "1077315655694",
+    appId: "1:1077315655694:web:757c1158c1ad417191c45f",
+    measurementId: "G-W1CMH97CG7"
+  }
+};
